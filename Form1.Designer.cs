9793248075bf1539779fd95d2a328df19a1488ce
Isaac Nana <odoyo029@gmail.com>
2022-02-18 15:19:47 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_UserName = new System.Windows.Forms.Label();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.txt_UserName = new System.Windows.Forms.TextBox();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.btn_LogIn = new System.Windows.Forms.Button();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.Check_Male = new System.Windows.Forms.CheckBox();
+            this.Check_Female = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lbl_UserName
+            // 
+            this.lbl_UserName.AutoSize = true;
+            this.lbl_UserName.Location = new System.Drawing.Point(39, 68);
+            this.lbl_UserName.Name = "lbl_UserName";
+            this.lbl_UserName.Size = new System.Drawing.Size(60, 13);
+            this.lbl_UserName.TabIndex = 0;
+            this.lbl_UserName.Text = "User Name";
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Location = new System.Drawing.Point(39, 117);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(52, 13);
+            this.lbl_Password.TabIndex = 1;
+            this.lbl_Password.Text = "password";
+            // 
+            // txt_UserName
+            // 
+            this.txt_UserName.Location = new System.Drawing.Point(142, 68);
+            this.txt_UserName.Name = "txt_UserName";
+            this.txt_UserName.Size = new System.Drawing.Size(198, 20);
+            this.txt_UserName.TabIndex = 2;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(142, 110);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(198, 20);
+            this.txt_Password.TabIndex = 3;
+            // 
+            // btn_LogIn
+            // 
+            this.btn_LogIn.Location = new System.Drawing.Point(142, 352);
+            this.btn_LogIn.Name = "btn_LogIn";
+            this.btn_LogIn.Size = new System.Drawing.Size(75, 23);
+            this.btn_LogIn.TabIndex = 4;
+            this.btn_LogIn.Text = "Log In";
+            this.btn_LogIn.UseVisualStyleBackColor = true;
+            this.btn_LogIn.Click += new System.EventHandler(this.btn_LogIn_Click);
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.Location = new System.Drawing.Point(265, 352);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(75, 23);
+            this.btn_Reset.TabIndex = 5;
+            this.btn_Reset.Text = "Reset";
+            this.btn_Reset.UseVisualStyleBackColor = true;
+            // 
+            // Check_Male
+            // 
+            this.Check_Male.AutoSize = true;
+            this.Check_Male.Location = new System.Drawing.Point(96, 203);
+            this.Check_Male.Name = "Check_Male";
+            this.Check_Male.Size = new System.Drawing.Size(49, 17);
+            this.Check_Male.TabIndex = 6;
+            this.Check_Male.Text = "Male";
+            this.Check_Male.UseVisualStyleBackColor = true;
+            this.Check_Male.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Check_Female
+            // 
+            this.Check_Female.AutoSize = true;
+            this.Check_Female.Location = new System.Drawing.Point(96, 244);
+            this.Check_Female.Name = "Check_Female";
+            this.Check_Female.Size = new System.Drawing.Size(60, 17);
+            this.Check_Female.TabIndex = 7;
+            this.Check_Female.Text = "Female";
+            this.Check_Female.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Check_Female);
+            this.Controls.Add(this.Check_Male);
+            this.Controls.Add(this.btn_Reset);
+            this.Controls.Add(this.btn_LogIn);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.txt_UserName);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_UserName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_UserName;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.TextBox txt_UserName;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Button btn_LogIn;
+        private System.Windows.Forms.Button btn_Reset;
+        private System.Windows.Forms.CheckBox Check_Male;
+        private System.Windows.Forms.CheckBox Check_Female;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_LogIn_Click(object sender, EventArgs e)
+        {
+            var userName = "isaac";
+            var password = txt_Password.Text.Trim();
+            if (txt_UserName.Text.Trim().ToLower() == userName && password == "12345")
+            {
+                new HomePage().ShowDialog();
+                Hide();
+            }
+             else
+            {
+                MessageBox.Show("wrong credentials, try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
